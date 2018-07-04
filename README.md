64afae109f443bfe40fb1f175554d7f79d555237
Iris <iriscarballo@gmail.com>
2018-07-04 16:47:26 -0600

    mobile view

---
+++ README.md
@@ -0,0 +1,1 @@
+// La decisión de poner el header como logo ha sido basada en la estrategia de diseño  mobile-first
---
+++ config.rb
@@ -0,0 +1,8 @@
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'assets/css'
+sass_dir = 'assets/css'
+javascripts_dir = 'assets/js'
+relative_assets = true
+line_comments = true
+# output_style = :compressed
